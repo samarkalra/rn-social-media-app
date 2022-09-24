3e39f1f02cccba5ef4e4c1a6bd6947c036d31079
samarkalra <skalra@ex2india.com>
2022-09-25 00:00:07 +0530

    Add image picker in create post screen

--- src/ui/screens/CreatePost/styles.ts
+++ src/ui/screens/CreatePost/styles.ts
@@ -8,13 +8,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  image: {
+  profileImage: {
     width: AVATAR_SIZE,
     height: AVATAR_SIZE,
     borderRadius: AVATAR_SIZE / 2,
   },
   name: { marginLeft: 10, fontWeight: '500', color: COLORS.black },
   buttonView: { marginTop: 'auto' },
+  imagePicker: { marginLeft: 'auto' },
+  imagePickerIcon: {
+    width: 30,
+    height: 30,
+  },
+  selectedImage: {
+    width: '50%',
+    aspectRatio: 4 / 3,
+    alignSelf: 'center',
+  },
 });
 
 export default styles;

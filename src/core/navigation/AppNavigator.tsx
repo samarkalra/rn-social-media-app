25b06a7acf7648fbaddcaa1dd81bad3f24990b13
samarkalra <skalra@ex2india.com>
2022-09-25 00:09:19 +0530

    Install @react-navigation/native-stack

---
+++ src/core/navigation/AppNavigator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <></>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
